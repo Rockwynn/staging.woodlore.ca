907de511823c3681c887086e780250ddea887303
Sandra White <whitehouseont@gmail.com>
2019-11-06 17:50:42 -0500

    replaced   semi colon for comma in lightbox.option script just before closing body tag, also changed window.onlod to window.on on line 705 woodlore.js, both caused errors in inspector

--- site/themes/woodlore/js/woodlore.js
+++ site/themes/woodlore/js/woodlore.js
@@ -685,7 +685,7 @@
 
 }(jQuery)
 $(function() {
-	
+
 	// Automatically add Zoom interaction
 	$('article.content img').attr('data-action', 'zoom');
 
@@ -702,7 +702,7 @@ $(function() {
 	search.focus().val(search.val());
 
 	// Fire up that gallery
-	$(window).load(function () {
+	$(window).on(function () {
 		collage();
 	});
 
@@ -725,9 +725,9 @@ function collage() {
 var resizeTimer = null;
 $(window).bind('resize', function() {
     $('.gallery-images img').css("opacity", 0);
-    
+
     if (resizeTimer) clearTimeout(resizeTimer);
-    
+
     resizeTimer = setTimeout(collage, 200);
 });
 //# sourceMappingURL=woodlore.js.map
